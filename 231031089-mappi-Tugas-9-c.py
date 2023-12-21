91b11b5dc00f5ea7752bf677cf9918c750e06e48
Andi Mappinawan <153783304+andimappi86-png@users.noreply.github.com>
2023-12-21 18:42:56 +0700

    Add files via upload

---
+++ 231031089-mappi-Tugas-9-c.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "Andi Mappinawan",
+      "kelas" : "SI23-C",
+      "nim" : "231031089",
+      "asal" : "Baringin",
+      "tanggal" : "8-Juni-2005",
+      "alamat" : "Btn Grand Sulawesi",
+      "hobi" : "Main game",
+      "tinggi" : "168 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
